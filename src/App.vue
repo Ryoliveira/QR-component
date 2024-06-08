d44a39cd89be0ff98594a30ffa0953fc7283fcf5
Ryan Oliveira <ryoliveira91@gmail.com>
2024-06-07 19:22:49 -0700

    Finished challenge

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import QRComponent from './components/QRComponent.vue';
+</script>
+
+<template>
+  <QRComponent />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/QRComponent.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <!-- main container-->
+  <div class="main-container">
+    <!-- QR Code -->
+    <div class="qr-container">
+      <img src="../assets/image-qr-code.png" alt="qr code" />
+    </div>
+
+    <!-- description text (bold)-->
+    <div class="description">Improve your front-end skills by building projects</div>
+
+    <!-- direction text -->
+    <div class="directions">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</div>
+  </div>
+</template>
+
+<style src="../scss/qrcomponent.scss" scoped></style>
