ce1bf1892999e4df110185a175b15ba1dd081f97
Ryan Oliveira <ryoliveira91@gmail.com>
2024-06-12 19:14:17 -0700

    refactored css and main component. added aria label and many other improvements

--- src/components/QRComponent.vue
+++ src/components/QRComponent.vue
@@ -1,20 +1,27 @@
 <script setup lang="ts">
+const frontendMentorLink: string = "https://www.frontendmentor.io/solutions/qr-component-using-padding-and-border-radius-with-a-bit-of-font-css-IrBrFHDvWs";
+const githubLink: string = "https://github.com/Ryoliveira";
 </script>
 
 <template>
   <!-- main container-->
-  <div class="main-container">
-    <!-- QR Code -->
-    <div class="qr-container">
-      <img src="../assets/image-qr-code.png" alt="qr code" />
-    </div>
+  <main>
+    <section class="main-container">
+      <!-- QR Code -->
+      <img class="qr-image" src="../assets/image-qr-code.png" alt="qr code to frontendmentor.io" />
 
-    <!-- description text (bold)-->
-    <div class="description">Improve your front-end skills by building projects</div>
+      <!-- description text (bold)-->
+      <h2 class="description">Improve your front-end skills by building projects</h2>
 
-    <!-- direction text -->
-    <div class="directions">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</div>
-  </div>
+      <!-- direction text -->
+      <p class="directions">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+    </section>
+  </main>
+
+  <footer>Frontend Mentor challenge
+     <a :href="frontendMentorLink" aria-label="frontend Mentor Solution" target="_blank">QR Component</a>. 
+     Coded by <a :href="githubLink" aria-label="github" target="_blank">Ryan Oliveira</a>
+    </footer>
 </template>
 
-<style src="../scss/qrcomponent.scss" scoped></style>
+<style src="../style/scss/qrcomponent.scss" scoped />
